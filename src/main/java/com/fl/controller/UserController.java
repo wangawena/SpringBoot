d8e59958593d553a132cf546aaf5475379d10a8c
Administrator <350646657@qq.com>
2022-11-15 14:56:52 +0800

    w

--- src/main/java/com/fl/controller/UserController.java
+++ src/main/java/com/fl/controller/UserController.java
@@ -5,6 +5,7 @@ import com.fl.pojo.Result;
 import com.fl.pojo.User;
 import com.fl.server.UserServer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.util.Random;
 
 @RestController
 @RequestMapping("/user")
@@ -22,7 +24,8 @@ public class UserController {
     @Autowired
     private UserServer userServer;
 
-    @PostMapping("/login")
+    //@PostMapping("/login")
+    @GetMapping("/login")
     public Result login(User user)
     {
         Result result=userServer.login(user);
@@ -30,10 +33,30 @@ public class UserController {
                 .getRequest().getSession();
 
         User u=(User)result.getData();
+        if(u.getAccount()!=null && u.getPassword()!=null)
+        {
+            session.setAttribute("account",u.getAccount());
+            session.setAttribute("password",u.getPassword());
+            System.out.println(session.getAttribute("account"));
+            System.out.println(session.getAttribute("password"));
+        }
         //if(!"".equals(u))
 
         return result;
     }
 
 
+    public String randomString()
+    {
+        String[] s={"1","2","3","4","5","6","7","8","9","0","A","B","C","D","E","F","G","H","I","J","K","L","M",
+                "N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+        int size=s.length;
+        String string="";
+        Random random=new Random();
+        for(int i=0;i<8;i++)
+        {
+            string+=s[random.nextInt(size)];
+        }
+        return string;
+    }
 }
--- src/main/java/com/fl/util/MyFilter.java
+++ src/main/java/com/fl/util/MyFilter.java
@@ -2,9 +2,13 @@ package com.fl.util;
 
 
 import com.fl.pojo.FilterTime;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebFilter(filterName ="firstFilter",urlPatterns ={"/*"})
@@ -23,6 +27,20 @@ public class MyFilter implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         filterTime.setStartTime(System.currentTimeMillis());
         System.out.println("进行过滤");
+
+        HttpSession session= ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes())
+                .getRequest().getSession();
+
+        String URI=((HttpServletRequest)servletRequest).getRequestURI();
+        System.out.println("URI:"+URI);
+
+        if("/user/login".compareTo(URI)!=0)
+        {
+            String account = session.getAttribute("account").toString();
+            String password = session.getAttribute("password").toString();
+
+            System.out.println("account:" + account + "\n" + "password:" + password);
+        }
         filterChain.doFilter(servletRequest,servletResponse);
         filterTime.setEndTime(System.currentTimeMillis());
         System.out.println("过滤器执行时间："+filterTime.getSubTime());
